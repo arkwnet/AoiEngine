47b0346b90fdbba736ac2e5d3821438fd03b575d
Sora Arakawa <sr@arkw.jp>
2021-04-01 17:53:28 +0900

    Add onload and initialize code

--- js/AoiEngine.js
+++ js/AoiEngine.js
@@ -3,3 +3,16 @@
 	Copyright (c) 2021 Sora Arakawa
 	Licensed under the MIT License
 */
+
+var canvas = null;
+var g = null;
+
+window.onload = function() {
+	AoiInit();
+}
+
+function AoiInit() {
+	canvas = document.getElementById("canvas");
+	g = canvas.getContext("2d");
+	g.fillRect(0, 0, 800, 480);
+}
