01e5a4eb7b4da68dc1fd85f0e4e51a25d4a6baea
Sora Arakawa <sr@arkw.jp>
2021-06-17 14:47:30 +0900

    Add AoiMain and AoiDrawSample

--- js/AoiEngine.js
+++ js/AoiEngine.js
@@ -1,11 +1,15 @@
 /*
-	AoiEngine
+	AoiEngine Ver.1.0 (Beta)
 	Copyright (c) 2021 Sora Arakawa
 	Licensed under the MIT License
 */
 
 var canvas = null;
 var g = null;
+var w = 800;
+var h = 480;
+var frame = 0;
+var time = 0;
 
 window.onload = function() {
 	AoiInit();
@@ -14,5 +18,23 @@ window.onload = function() {
 function AoiInit() {
 	canvas = document.getElementById("canvas");
 	g = canvas.getContext("2d");
-	g.fillRect(0, 0, 800, 480);
+	AoiMain();
+}
+
+function AoiMain() {
+	g.fillStyle = "black";
+	g.fillRect(0, 0, w, h);
+	AoiDrawSample();
+	frame++;
+	if (frame >= 60) {
+		frame = 0;
+		time++;
+	}
+	requestAnimationFrame(AoiMain);
+}
+
+function AoiDrawSample() {
+	g.fillStyle = "red";
+	g.fillText(frame, 20, 20);
+	g.fillText(time, 20, 60);
 }
